85bf019d450d430538d8eb88445c9844a9ad2f1c
Esteban Cabrera <esteban.cabrera@pucp.edu.pe>
2024-08-24 00:14:33 -0500

    update
    - scrapper updated
    - test added
    - README file updated

--- README.md
+++ README.md
@@ -1,2 +1,12 @@
-# bol-scrapping
-This repository allows the scrapping of the Bol.com website using BeatifulSoup
+# buscalibre-scrapping
+This repository allows the scrapping of the **[buscalibre.pe](https://www.buscalibre.pe/)** website using BeatifulSoup. The user inputs the topic of the books they are searching for. The code scraps prices (before and after a discount) and returns the cheapest book in that topic as well as the book with the highest discount. 
+
+## 📚 Structure
+The repository is organized as follows:
+
+- `buscalibre_scrapper.py`: This script contains the code for the project.
+
+## 👨‍🏫 Author
+
+This code was created by Esteban Cabrera. If you have any questions or suggestions, feel free to reach out.
+- **Email:** [esteban.cabrera@pucp.edu.pe](mailto:esteban.cabrera@pucp.edu.pe)
---
+++ buscalibre_scrapper.py
@@ -0,0 +1,68 @@
+import requests
+import pandas as pd
+import numpy as np
+from bs4 import BeautifulSoup as bs
+
+# URL to scrape
+url = "https://www.buscalibre.pe/libros/search/?q="
+
+# Search the topic you prefer
+user_input = input("What topic/author are you interested in?: ")
+
+url_final = url + user_input
+
+response = requests.get(url_final)
+soup = bs(response.text, "html.parser")
+
+headers = {
+    "User-Agent": "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/58.0.3029.110 Safari/537.36"
+}
+
+response = requests.get(url_final, headers=headers)
+
+# Verify an OK response
+if response.status_code == 200:
+    soup = bs(response.text, "html.parser")
+    print(f"You have searched for the topic {user_input}")  # Imprime el HTML de manera legible
+else:
+    print(f"Error {response.status_code}: No se pudo acceder al sitio web")
+
+titles = []
+prices_before = []
+prices_now = []
+discounts = []
+
+for objeto in soup.find_all("div", class_="box-producto"):
+    title = objeto.find("h3").text.strip()
+    price_before = objeto.find("p", class_="precio-antes").text.strip()
+    price_before = price_before.replace("S/  ", "")
+    price_before = price_before.replace(",", ".")
+    price_before = float(price_before)
+    price_now = objeto.find("p", class_="precio-ahora").text.strip()
+    price_now = price_now.replace("S/  ", "")
+    price_now = price_now.replace(",", ".")
+    price_now = float(price_now)
+
+    discount = round((price_before - price_now) / price_before * 100, 2)
+    
+    titles.append(title)
+    prices_before.append(price_before)
+    prices_now.append(price_now)
+    discounts.append(discount)
+
+df = pd.DataFrame({
+    "Title" : titles,
+    "Prices before" : prices_before,
+    "Prices now" : prices_now,
+    "Discount" : discounts
+})
+
+# Get the book with the minimum price
+min_price_book = df[df['Prices now'] == df['Prices now'].min()]
+
+# Get the book with the maximum discount
+max_discount_book = df[df['Discount'] == df['Discount'].max()]
+
+# Print the data
+print(f"The book with the minimum price is {min_price_book.iloc[0, 0]} with a cost of S/{min_price_book.iloc[0, 2]}")
+print(f"The book with the maximum discount is {max_discount_book.iloc[0, 0]} with a discount of {max_discount_book.iloc[0, 3]}% and a cost of S/{max_discount_book.iloc[0, 2]}")
